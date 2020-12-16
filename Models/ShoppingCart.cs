e70c32380f8146ba0e2bce7cfd2217816c40052d
Hoai Bao <hoaibao08062000@gmail.com>
2020-12-16 18:29:11 +0700

    Commit Changes

---
+++ Models/ShoppingCart.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Spice.Models {
+    public class ShoppingCart {
+        
+        [Key]
+        public int CartId { get; set; }
+        public string UserId { get; set; }
+        [NotMapped]
+        [ForeignKey("UserId")]
+        public ApplicationUser User { get; set; }
+
+        public int MenuItemId { get; set; }
+        [NotMapped]
+        [ForeignKey("MenuItemId")]
+        public MenuItem MenuItem { get; set; }
+        [Range(1,Int32.MaxValue,ErrorMessage = "Count must equal or greater than 1")]
+        public int Count { get; set; }
+
+        public ShoppingCart() {
+            Count = 0;
+        }
+    }
+}
